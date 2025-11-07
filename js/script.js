2baf89c669e1e0effbf74ea8f5a2cea20b585d3b
SENAI Zerbini <aluno@escolar509.sp.senai.br>
2025-11-07 17:06:28 -0300

    Adicionei os Eventos no JavaScript

---
+++ js/script.js
@@ -0,0 +1,30 @@
+let events = [
+     { id: "E01",
+        titulo: "Cybersecurity",
+        categoria: "Palestra",
+        dataISO: "05/12/2025",
+        local: "Auditório Senai",
+        descricao: "Palestra de concientização sobre Cybersegurança",
+        destaque: "Aberto"
+    },
+
+    { id: "E02",
+        titulo: "Segurança no Trabalho",
+        categoria: "Palestra",
+        dataISO: "20/11/2025",
+        local: "Auditório Senai",
+        descricao: "Palestra de concientização sobre Segurança no Trabalho",
+        destaque: "Lotado"
+    },
+
+    { id: "E03",
+        titulo: "Senai Portas Abertas - 30 Anos",
+        categoria: "Comemoração",
+        dataISO: "01/12/2025",
+        local: "Campus Senai",
+        descricao: "Evento Portas Abertas em comemoração aos 30 anos do Senai Zerbini",
+        destaque: "Aberto"
+    }
+
+    
+   ];
